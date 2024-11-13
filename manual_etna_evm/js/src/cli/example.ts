7bbb089710a63c7cf24c15bc5d0cef493d08e110
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-11-13 06:49:42 +0000

    rename the key for clarity

--- manual_etna_evm/js/src/cli/example.ts
+++ manual_etna_evm/js/src/cli/example.ts
@@ -4,7 +4,7 @@ import { addTxSignatures, Context, evm, utils } from '@avalabs/avalanchejs';
 
 const C_CHAIN_ADDRESS = "0x700046af78cD0E55D5d25025bCEaE992B87A5686"
 const X_CHAIN_ADDRESS = "X-custom125uzjyu824gwlz6e4jqthvks9er6xwvx7ve80v"
-const PRIVATE_KEY = "0963764d0da7b2f6a4327eb49042bf9e05e88534f22acb9fbf469eb44a8ed3f8"
+const TEST_PRIVATE_KEY = "0963764d0da7b2f6a4327eb49042bf9e05e88534f22acb9fbf469eb44a8ed3f8"
 
 const main = async (publicUrl: string) => {
     const provider = new JsonRpcProvider(publicUrl + '/ext/bc/C/rpc');
@@ -31,7 +31,7 @@ const main = async (publicUrl: string) => {
 
     await addTxSignatures({
         unsignedTx: tx,
-        privateKeys: [utils.hexToBuffer(PRIVATE_KEY)],
+        privateKeys: [utils.hexToBuffer(TEST_PRIVATE_KEY)],
     });
 
     await evmapi.issueSignedTx(tx.getSignedTx());
