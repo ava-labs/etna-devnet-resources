6bec220d99c5a330112c4d05faf97a21fed6cb0e
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-12-03 15:03:38 +0000

    Restore compilation

---
+++ manual_etna_evm/07_compile_validator_manager/compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+TELEPORTER_COMMIT=790ccce873f9a904910a0f3ffd783436c920ce97
+
+docker build -t validator-manager-compiler --build-arg TELEPORTER_COMMIT=$TELEPORTER_COMMIT "$SCRIPT_DIR"
+docker run -it --rm -v "$SCRIPT_DIR/bindings":/bindings -v "$SCRIPT_DIR/compiled":/compiled -v "$SCRIPT_DIR/teleporter":/teleporter -e TELEPORTER_COMMIT=$TELEPORTER_COMMIT validator-manager-compiler
