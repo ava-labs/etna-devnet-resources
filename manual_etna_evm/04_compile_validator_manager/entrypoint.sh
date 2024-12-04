2d3ef09b1438f59e38ecacce905ae6579ded0153
Containerman17 <8990432+containerman17@users.noreply.github.com>
2024-12-04 02:11:10 +0000

    04_compile_validator_manager

---
+++ manual_etna_evm/04_compile_validator_manager/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -exu
+
+if [ ! -d "/teleporter_src/contracts" ]; then
+    git clone https://github.com/ava-labs/teleporter /teleporter_src && \
+    cd /teleporter_src && \
+    git checkout $TELEPORTER_COMMIT
+fi
+
+cd /teleporter_src/contracts && forge build --extra-output-files=bin
+
+
+# Extract ABI from the compiled JSON file
+jq .abi /teleporter_src/out/PoAValidatorManager.sol/PoAValidatorManager.json > /teleporter_src/out/PoAValidatorManager.sol/PoAValidatorManager.abi
+jq .abi /teleporter_src/out/PoSValidatorManager.sol/PoSValidatorManager.json > /teleporter_src/out/PoSValidatorManager.sol/PoSValidatorManager.abi
+
+mkdir -p "/bindings/povalidatormanager"
+mkdir -p "/bindings/posvalidatormanager"
+abigen \
+    --abi "/teleporter_src/out/PoAValidatorManager.sol/PoAValidatorManager.abi" \
+    --pkg povalidatormanager \
+    --type PoAValidatorManager \
+    --out "/bindings/povalidatormanager/PoAValidatorManager.go" \
+    --bin "/teleporter_src/out/PoAValidatorManager.sol/PoAValidatorManager.bin"
+
+abigen \
+    --abi "/teleporter_src/out/PoSValidatorManager.sol/PoSValidatorManager.abi" \
+    --pkg posvalidatormanager \
+    --type PoSValidatorManager \
+    --out "/bindings/posvalidatormanager/PoSValidatorManager.go" \
+    --bin "/teleporter_src/out/PoSValidatorManager.sol/PoSValidatorManager.bin"
+
+cp -r /teleporter_src/out/PoAValidatorManager.sol/*.json /compiled/
+cp -r /teleporter_src/out/PoSValidatorManager.sol/*.json /compiled/
+
+chown -R $HOST_UID:$HOST_GID /bindings /compiled
--- manual_etna_evm/run.sh
+++ manual_etna_evm/run.sh
@@ -1,41 +1,41 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -euo pipefail
+# set -euo pipefail
 
-echo -e "\n🔑 Generating keys\n"
-go run ./01_generate_keys/
+# echo -e "\n🔑 Generating keys\n"
+# go run ./01_generate_keys/
 
-echo -e "\n💰 Checking balance\n" 
-go run ./02_check_balance/
+# echo -e "\n💰 Checking balance\n" 
+# go run ./02_check_balance/
 
-echo -e "\n🕸️  Creating subnet\n"
-go run ./03_create_subnet/
+# echo -e "\n🕸️  Creating subnet\n"
+# go run ./03_create_subnet/
 
-echo -e "\n🧱 Generating genesis\n"
-go run ./04_L1_genesis/
+# echo -e "\n🧱 Generating genesis\n"
+# go run ./04_L1_genesis/
 
-echo -e "\n⛓️  Creating chain\n"
-go run ./05_create_chain/
+# echo -e "\n⛓️  Creating chain\n"
+# go run ./05_create_chain/
 
-echo -e "\n🚀 Launching nodes\n"
-./06_launch_nodes/launch.sh
+# echo -e "\n🚀 Launching nodes\n"
+# ./06_launch_nodes/launch.sh
 
-echo -e "\n🎯 Activate ProposerVM fork\n"
-go run ./10_activate_proposer_vm/
+# echo -e "\n🎯 Activate ProposerVM fork\n"
+# go run ./10_activate_proposer_vm/
 
-echo -e "\n📦 Deploy Validator Manager\n"
-go run ./07_depoly_validator_manager/
+# echo -e "\n📦 Deploy Validator Manager\n"
+# go run ./07_depoly_validator_manager/
 
-echo -e "\n🔮 Converting chain into L1\n"
-go run ./08_convert_chain/
+# echo -e "\n🔮 Converting chain into L1\n"
+# go run ./08_convert_chain/
 
-echo -e "\n🔃 Restarting nodes\n"
-./06_launch_nodes/launch.sh # Reuse the script to restart nodes
+# echo -e "\n🔃 Restarting nodes\n"
+# ./06_launch_nodes/launch.sh # Reuse the script to restart nodes
 
-echo -e "\n🔌 Initialize Validator Manager\n"
-go run ./11_validator_manager_initialize/ 
+# echo -e "\n🔌 Initialize Validator Manager\n"
+# go run ./11_validator_manager_initialize/ 
 
-echo -e "\n👥 Initialize validator set\n"
-go run ./12_initialize_validator_set
+# echo -e "\n👥 Initialize validator set\n"
+# go run ./12_initialize_validator_set
 
 
