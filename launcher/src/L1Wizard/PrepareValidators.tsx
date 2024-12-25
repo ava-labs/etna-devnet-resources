548789622817c0455ff2b7e5b8fc3491f17c7af0
Martin Eckardt <martin.eckardt@avalabs.org>
2024-12-25 12:57:45 +0100

    Simplify node count selector

--- launcher/src/L1Wizard/PrepareValidators.tsx
+++ launcher/src/L1Wizard/PrepareValidators.tsx
@@ -13,7 +13,12 @@ import NextPrev from './ui/NextPrev';
 
 export default function PrepareValidators() {
     const { nodesCount, setNodesCount } = useWizardStore();
-    const nodeCounts = [1, 3, 5];
+
+    const nodeConfigurations = {
+        1: { colorClass: 'bg-red-100 text-red-800', badge: 'Dev' },
+        3: { colorClass: 'bg-green-100 text-green-800', badge: 'Testnet' },
+        5: { colorClass: 'bg-blue-100 text-blue-800', badge: 'Mainnet' },
+    };
 
     return <>
         <h1 className="text-2xl font-medium mb-6">
@@ -52,38 +57,30 @@ export default function PrepareValidators() {
 
         <h3 className="mb-4 font-medium">How many nodes do you want to run?</h3>
         <ul className="mb-4 items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex">
-            {nodeCounts.map((count) => (
-                <li key={count} className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r last:border-r-0 ">
-                    <div className="flex items-center ps-3">
-                        <input
-                            id={`nodes-${count}`}
-                            type="radio"
-                            checked={nodesCount === count}
-                            onChange={() => setNodesCount(count)}
-                            name="nodes-count"
-                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-0"
-                        />
-                        <label htmlFor={`nodes-${count}`} className="w-full py-3 ms-2 text-sm font-medium text-gray-900">
-                            {count} {count === 1 ? 'Node' : 'Nodes'}
-                            {count === 1 && (
-                                <span className="ms-2 bg-red-100 text-red-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
-                                    Dev
-                                </span>
-                            )}
-                            {count === 3 && (
-                                <span className="ms-2 bg-green-100 text-green-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
-                                    Testnet
-                                </span>
-                            )}
-                            {count === 5 && (
-                                <span className="ms-2 bg-blue-100 text-blue-800 text-xs font-medium px-2.5 py-0.5 rounded-full">
-                                    Mainnet
-                                </span>
-                            )}
-                        </label>
-                    </div>
-                </li>
-            ))}
+            {Object.entries(nodeConfigurations).map(([count, config]) => {
+                return (
+                    <li key={count} className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r last:border-r-0 ">
+                        <div className="flex items-center ps-3">
+                            <input
+                                id={`nodes-${count}`}
+                                type="radio"
+                                checked={nodesCount === Number(count)}
+                                onChange={() => setNodesCount(Number(count))}
+                                name="nodes-count"
+                                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-0"
+                            />
+                            <label htmlFor={`nodes-${count}`} className="w-full py-3 ms-2 text-sm font-medium text-gray-900">
+                                {count} {count === '1' ? 'Node' : 'Nodes'}
+                                {config.badge && (
+                                    <span className={`ms-2 ${config.colorClass} text-xs font-medium px-2.5 py-0.5 rounded-full`}>
+                                        {config.badge}
+                                    </span>
+                                )}
+                            </label>
+                        </div>
+                    </li>
+                );
+            })}
         </ul>
 
         <NextPrev nextDisabled={!nodesCount} currentStepName="prepare-validators" />
